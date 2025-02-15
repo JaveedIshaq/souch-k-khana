eca5c50534d6976b0fe35c8c3496528350b049ed
Javeed Ishaq <ishaqjaveed1@gmail.com>
2025-02-15 07:36:41 +0500

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String apiKey = "ADD_YOUR_GOOGLE_GEMEI_API_KEY";
+const String apiKey = "ADD_YOUR_GOOGLE_GEMINI_KEY_HERE";
 
 Color themeColor = Color(0XFFFFA726);
 Color themeDarkColor = const Color(0xFFf76e05);
